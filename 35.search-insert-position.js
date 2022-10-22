ef5bc7deffc8979e290308bdac5dde86d614c4f5
nnhjs <nnhungjs@gmail.com>
2022-10-22 22:53:38 +0700

    initial

---
+++ 35.search-insert-position.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=35 lang=javascript
+ *
+ * [35] Search Insert Position
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+  const len = nums.length;
+  if (len === 0) return 0;
+  if (target > nums[len - 1]) return len;
+  if (target < nums[0]) return 0;
+  let left = 0;
+  let right = len - 1;
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) return mid;
+    if (nums[mid] > target) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return left;
+};
+
+// @lc code=end
+
