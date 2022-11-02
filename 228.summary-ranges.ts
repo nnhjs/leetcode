b34facf7e78d5887f3c0d38b213132d508a389f2
nnhjs <nnhungjs@gmail.com>
2022-11-02 23:51:36 +0700

    7/100

---
+++ 228.summary-ranges.ts
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=228 lang=typescript
+ *
+ * [228] Summary Ranges
+ */
+
+// @lc code=start
+function summaryRanges(nums: number[]): string[] {
+  const result: string[] = [];
+  let current = [];
+  for (let i = 0; i < nums.length; i++) {
+    if(nums[i] + 1 === nums[i+1]) {
+      
+    }
+  }
+  return result;
+};
+// @lc code=end
+
