81ce8a1589275092df7bd2b1439a24233dc3abb2
nnhjs <nnhungjs@gmail.com>
2022-11-03 00:11:46 +0700

    8/100

--- 228.summary-ranges.ts
+++ 228.summary-ranges.ts
@@ -6,14 +6,33 @@
 
 // @lc code=start
 function summaryRanges(nums: number[]): string[] {
-  const result: string[] = [];
-  let current = [];
-  for (let i = 0; i < nums.length; i++) {
-    if(nums[i] + 1 === nums[i+1]) {
-      
-    }
+  const resultArr: number[][] = [];
+  let smallestArr: number[] = []
+
+  if(nums.length === 0) return []
+  
+  smallestArr[0] = nums[0]
+
+  for (let i = 1; i < nums.length; i++) {
+    if(smallestArr[smallestArr.length-1] + 1 === nums[i]) {
+      smallestArr.push(nums[i]);
+    } else {
+      resultArr.push(smallestArr)
+      smallestArr = [nums[i]];
+    } 
   }
-  return result;
+
+  resultArr.push(smallestArr)
+
+  const resultStr = resultArr.map(r => {
+    if(r.length === 1) {
+      return `${r[0]}`
+    } else {
+      return `${r[0]}->${r[r.length-1]}`
+    }
+  })
+
+  return resultStr
 };
 // @lc code=end
 
